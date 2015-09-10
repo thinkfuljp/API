f9750df3e0f80dbd9a972442bc443d5bb16c63b8
thinkfuljp <jpdevries@thinkful.com>
2015-09-10 09:35:58 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,50 +1,50 @@
-var requestURL = 'https://www.googleapis.com/youtube/v3/search';
-
 $(function() {
-    console.log('ready');
+
+  $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data) {
+    myData = data.Search;
+    showResults(data.Search);
+    $.each(myData, function(index, value) {
+      console.log(value.Title);
+    });
+  });
 
     $('#search-term').submit(function(event) {
-        event.preventDefault();
-        var searchTerm = $('#query').val();
-
-
-        function getRequest(searchTerm) {
-            $.getJSON('https://www.googleapis.com/youtube/v3/search' + searchTerm + '&r=json', function(data) {
-                getRequest(searchTerm);
-                console.log($(this).find('input').val());
-            });
-            var params = {
-                s: searchTerm,
-                r: 'json'
-            };
-            url = 'https://www.googleapis.com/youtube/v3/searchs';
-
-            $.getJSON(requestURL, params, function(data) {
-                value = data.val;
-                myData = data.Search;
-                showResults(data.Search);
-                $.each(myData, function(index, value) {
-                    console.log(value.Title);
-                });
-            });
-        };
-
-        $.getJSON('https://www.googleapis.com/youtube/v3/search', {
-                part: 'snippet',
-                key: 'AIzaSyD27ABFHFCuTG5zoQd_9D0Yx3KmUMY_TWI',
-                q: "",
-
-            },
-
-            function showResults(results) {
-                var html = "";
-                $.each(results, function(index, value) {
-                    html += '<p>' + value.Title + '</p>';
-
-                });
-                $('#search-results').html(html);
-
-            });
-
+      event.preventDefault();
+      var searchTerm = $('#query').val();
+      $.getJSON('http://www.omdbapi.com/?s=' + searchTerm + '&r=json', function(data) {
+        getRequest(searchTerm); 
+      });
     });
-});
+
+  function getRequest(searchTerm){
+    console.log('getRequest: '+ searchTerm);
+    var params = {
+      s: searchTerm,
+      r: 'json'
+    };
+    url = 'http://www.omdbapi.com/';
+
+    $.getJSON(url, params, function(data) {
+      showResults(data.Search);
+    });
+  }
+
+    function showResults(results) {
+        console.log('showResults');
+     var html = "";
+      $.each(results, function(index, value) {
+       html += '<p>' + value.Title + '</p>';
+        console.log(value.Title);
+    }); 
+
+    $('#search-results').html(html);
+
+  };
+
+})
+
+
+
+
+
+
